74cf60fa501141348d3860479319a5dc698b05c6
朱敏 <zhumin@qtone.cn>
2020-01-14 12:28:34 +0800

    zk

--- zk/synczk-r(pri).py
+++ zk/synczk-r(pri).py
@@ -11,11 +11,11 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 def readzk():
-    date=time.strftime('%Y%m%d',time.localtime())
+    #date=time.strftime('%Y%m%d',time.localtime())
     database="qky_ops"
     con=MySQLdb.connect(host='10.10.200.11',user='admin',passwd='',db='qky_ops',charset="utf8")
     cursor=con.cursor()
-    delesql="delete from qky_ops.zk_node"
+    delesql="delete from qky_ops.zk_node where path like '/configs%' or path like '/startconfigs%'"
     cursor.execute(delesql)
     con.commit()
     zk_hosts="10.10.200.11:2181"
@@ -23,38 +23,58 @@ def readzk():
     zk=KazooClient(zk_hosts)
     zk.start()
 
-    zknod1="/configs/qky"
-    zknod2="/startconfigs/qky"
-    children=zk.get_children(zknod1)
-    childrens=zk.get_children(zknod2)
-    for chil in children:
-        chil2=os.path.join(zknod1,chil)
-        if zk.exists(chil2):
-            children2=zk.get_children(chil2)
-            for chil in children2:
-                chil3=os.path.join(chil2,chil)
-                if zk.exists(chil3):
-                    children3=zk.get_children(chil3)
-                    for chil in children3:
-                        chil4=os.path.join(chil3,chil)
-                        data,stat=zk.get(chil4)
-                        updatsql="INSERT INTO qky_ops.zk_node values('%s','%s');"%(chil4,data)
-                        cursor.execute(updatsql)
-                        con.commit()
-    for chil21 in childrens:
-        chil22=os.path.join(zknod2,chil21)
-        if zk.exists(chil22):
-            childrens2=zk.get_children(chil22)
-            for chil23 in childrens2:
-                chil24=os.path.join(chil22,chil23)
-                data1,stat=zk.get(chil24)
-                updatsql1="INSERT INTO qky_ops.zk_node values('%s','%s');"%(chil24,data1)
-                cursor.execute(updatsql1)
-                con.commit()
+    dynamic="/configs"
+    startconfig="/startconfigs"
+    dynodes=zk.get_children(dynamic)
+    stnodes=zk.get_children(startconfig)
 
+    #获取启动配置节点
+    if len(stnodes)!=0:
+        for stnode in stnodes:
+            stnode=os.path.join(startconfig,stnode)
+            secondestnodes=zk.get_children(stnode)
+            if len(secondestnodes)!=0:
+                for secondestnode in secondestnodes:
+                    thirdnodes=os.path.join(stnode,secondestnode)
+                    fourthnodes=zk.get_children(thirdnodes)
+                    if len(fourthnodes)!=0:
+                        for fourthnode in fourthnodes:
+                            lastnode=os.path.join(thirdnodes,fourthnode)
+                            data2,stat=zk.get(lastnode)
+                            insertsql2="INSERT INTO qky_ops.zk_node(appCode,path,content) values('%s','%s','%s');"%(secondestnode,lastnode,data2)
+                            cursor.execute(insertsql2)
+                            con.commit()
+                            
+    else:
+        print "null"
+
+    #获取动态配置节点
+    if len(dynodes)!=0:
+        for dynode in dynodes:
+            dynode=os.path.join(dynamic,dynode)
+            secondestdynodes=zk.get_children(dynode)
+            if len(secondestdynodes)!=0:
+                for secondestdynodena in secondestdynodes:
+                    thirddynodes=os.path.join(dynode,secondestdynodena)
+                    fourthdynodes=zk.get_children(thirddynodes)
+                    if len(fourthdynodes)!=0:
+                        for fourthdynode in fourthdynodes:
+                            fifthdyndodes=os.path.join(thirddynodes,fourthdynode)
+                            sixthdynodes=zk.get_children(fifthdyndodes)
+                            if len(sixthdynodes)!=0:
+                                for sixthdynode in sixthdynodes:
+                                    seventhdynodes=os.path.join(fifthdyndodes,sixthdynode)
+                                    data1,stat1=zk.get(seventhdynodes)
+                                    insertsql1="INSERT INTO qky_ops.zk_node(appCode,path,content) values('%s','%s','%s');"%(secondestdynodena,seventhdynodes,data1)
+                                    cursor.execute(insertsql1)
+                                    con.commit()
+                            else:
+                                data,stat=zk.get(fifthdyndodes)
+                                insertsql="INSERT INTO qky_ops.zk_node(appCode,path,content) values('%s','%s','%s');"%(secondestdynodena,fifthdyndodes,data)
+                                cursor.execute(insertsql)
+                                con.commit()
+    zk.stop()
     cursor.close()
     con.close()
-    
-    zk.stop()
     print "读取完成......"
 readzk()
--- zk/zk_node.sql
+++ zk/zk_node.sql
@@ -1,5 +1,7 @@
 CREATE TABLE `zk_node` (
+   `id`  bigint(20)NOT NULL AUTO_INCREMENT,
+  `appCode`	 varchar(255) DEFAULT NULL,
   `path` varchar(255) DEFAULT NULL,
-  `content` varchar(5000) DEFAULT NULL
-
-) ENGINE=InnoDB AUTO_INCREMENT=11715 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+  `content` varchar(5000) DEFAULT NULL,
+  primary key(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
