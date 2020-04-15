76cb8c4f6761078724a6fa0dcecf8db2f4ded96b
朱敏 <zhumin@qtone.cn>
2020-04-15 08:19:12 +0800

    修改jobversion

--- sendjenkins/jobversion.py
+++ sendjenkins/jobversion.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #coding:utf-8
 
 import urllib2
@@ -6,25 +7,32 @@ import sys
 import jenkins
 import time
 
-def get_job_version(job_name):
-    server = jenkins.Jenkins('url', username='', password='')
-    if server.job_exists(name=job_name):
-
-        jobInfo = server.get_job_info(job_name)
-        lastBuildNumber = server.get_job_info(job_name)['lastBuild']['number']
-        Buildtemp = max(server.get_build_info(job_name, lastBuildNumber)['actions'])
-        Buildversion=Buildtemp['lastBuiltRevision']['SHA1']
-        tempTime=server.get_build_info(job_name,lastBuildNumber)['timestamp']
-        buildtimestamp=int(str(tempTime)[0:10])
-        timeArray = time.localtime(buildtimestamp)
-        buildTime=time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-        print "版本号:",Buildversion
-        print "构建时间:",buildTime
-    else:
-        print job_name,"不存在，请检查job名称，重新输入执行..."
+def get_job_version(na):
+    server = jenkins.Jenkins('jkurl', username='', password='')
+    jobs_list=server.get_all_jobs()
+    for jobs in jobs_list:
+        job_name_list=jobs['fullname']
+        if job_name_list.startswith("qky-"):
+            job_name=job_name_list
+            if na in job_name:
+                if server.job_exists(name=job_name):
+                    jobInfo = server.get_job_info(job_name)
+                    if server.get_job_info(job_name)['lastBuild'] is not None:
+                        lastBuildNumber = server.get_job_info(job_name)['lastBuild']['number']
+                        Buildtemp = max(server.get_build_info(job_name, lastBuildNumber)['actions'])
+                        if Buildtemp.has_key('lastBuiltRevision'):
+                            Buildversion=Buildtemp['lastBuiltRevision']['SHA1']
+                            tempTime=server.get_build_info(job_name,lastBuildNumber)['timestamp']
+                            buildtimestamp=int(str(tempTime)[0:10])
+                            timeArray = time.localtime(buildtimestamp)
+                            buildTime=time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+                            print "项目名称:",job_name
+                            print "版本号:",Buildversion
+                            print "构建时间:",buildTime
+                else:
+                    print job_name,"不存在，请检查job名称，重新输入执行..."
 
 
 if __name__=='__main__':
-    args1 = sys.argv[1]
-    job_name="qky-"+args1
-    get_job_version(job_name)
+    na = sys.argv[1]
+    get_job_version(na)
