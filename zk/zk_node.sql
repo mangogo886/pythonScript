3e8f818151daa299df71e8a30e52955be24ef222
朱敏 <zhumin@qtone.cn>
2020-04-26 15:03:35 +0800

    修改zksql

--- zk/zk_node.sql
+++ zk/zk_node.sql
@@ -4,6 +4,6 @@ CREATE TABLE `zk_node` (
   `path` varchar(255) DEFAULT NULL,
   `content` varchar(5000) DEFAULT NULL,
   `mark`  int(1) DEFAULT NULL,
-  `update_time`  datetime NOT NULL,
+  `update_time`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   primary key(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
