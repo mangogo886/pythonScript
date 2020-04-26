b17f88d803ca0126fd8e5a25dc35279344213e7c
朱敏 <zhumin@qtone.cn>
2020-04-26 14:39:43 +0800

    更新sql

--- zk/zk_node.sql
+++ zk/zk_node.sql
@@ -3,5 +3,7 @@ CREATE TABLE `zk_node` (
   `appCode`	 varchar(255) DEFAULT NULL,
   `path` varchar(255) DEFAULT NULL,
   `content` varchar(5000) DEFAULT NULL,
+  `mark`  int(1) DEFAULT NULL,
+  `update_time`  datetime NOT NULL,
   primary key(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
