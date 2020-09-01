0129f65821ded2514fdf9729d02343a91191d9fc
zhumin <zhum98@126.com>
2020-09-01 11:01:13 +0800

    优化modifyjkjob.py

--- privatedeploy/modifyng.py
+++ privatedeploy/modifyng.py
@@ -7,7 +7,7 @@ import os
 
 
 configPath="config.ini"
-ngconfig="/data/privatedep/sites-available/pro"
+ngconfig="/data/prideploy/ng/sites-available/pro"
 filesuffix=".qky100.com.conf"
 oldip="99.99.99.99:9999"
 httpPort = 8080
@@ -21,9 +21,9 @@ for ip in iplist:
     opts=cf.get(ip,'app')
     optsSuffix = opts.split(',')
     for i,suf in enumerate(optsSuffix):
-        #print ip,i,suf,httpPort++i+1
-        a=suf[4:]
+        a=suf.replace('qky-','')
         joinfile=a+filesuffix
+        h5file=a+"-h5"+filesuffix
         os.chdir(ngconfig)
         if os.path.exists(joinfile):
             port=httpPort++i+1
@@ -31,3 +31,11 @@ for ip in iplist:
             print "%s is replaced by %s:%s in the %s"%(oldip,ip,port,joinfile)
         else:
             print "%s %s does not exist"%(ip,joinfile)
+
+
+        if os.path.exists(h5file):
+            port=httpPort++i+1
+            os.system('sed -i "s/%s/%s:%s/g" %s'%(oldip,ip,port,h5file))
+            print "%s is replaced by %s:%s in the %s"%(oldip,ip,port,h5file)
+        else:
+            print "%s %s does not exist"%(ip,h5file)
