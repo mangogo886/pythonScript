71c1ac810d15e7ba4950ce07f8b88f07fcc0929d
zhumin <zhum98@126.com>
2020-10-20 11:56:03 +0800

    修改batchBuildApp.py

--- sendjenkins/sendjkbuild/batchBuildApp/batchBuildApp.py
+++ sendjenkins/sendjkbuild/batchBuildApp/batchBuildApp.py
@@ -20,46 +20,66 @@ def buildjob():
     for list in envlist:
         listname=envname(list)
         jkurl = cf.get(list, 'url')
-
         try:
             server = jenkins.Jenkins(jkurl, username=user, password=passwd,timeout =20)
             if server.job_exists(name=jobName):
+                #获取job信息
                 jobinfo = server.get_job_info(jobName)
+                #判断是否设置了构建参数
                 if jobinfo['actions'][0]:
-                    server.build_job(jobName,parameters=param_dict)
-                    lastBuildNumber = server.get_job_info(jobName)['lastBuild']['number']
-                    buil = server.get_build_info(jobName, lastBuildNumber)['building']
-                    # 判断是否正在构建
+                    #执行build
                     print ("\033[1;34m %s %s 开始构建..... \033[0m"%(listname,jobName))
-                    while buil:
-                        time.sleep(1)
-                        buil = server.get_build_info(jobName, lastBuildNumber)['building']
+                    server.build_job(jobName,parameters=param_dict)
+                    queue_job=server.get_queue_info()
+                    #判断执行的job是否处于pending状态
+                    while queue_job:
+                        queue_job=server.get_queue_info()
+                        for queue_building_job in queue_job:
+                            queue_name=queue_building_job['task']['name']
+                            if queue_name!=jobName:
+                                break
 
+                    lastBuildNumber = server.get_job_info(jobName)['lastBuild']['number']
+                    #获取实时构建输出日志
+                    buil = server.get_build_info(jobName, lastBuildNumber)['building']
+                    while buil:
+                            time.sleep(1)
+                            buil = server.get_build_info(jobName, lastBuildNumber)['building']
                     console = server.get_build_console_output(jobName, lastBuildNumber)
+                    print ("\033[1;34m %s 构建序号：%s..... \033[0m"%(listname,lastBuildNumber))
                     print console
                     print ("\033[1;34m %s %s 构建成功..... \033[0m"%(listname,jobName))
+           
                 else:
                     print ("\033[1;34m %s %s 无参数构建.... \033[0m"%(listname,jobName))
-                    server.build_job(jobName)
-                    lastBuildNumber = server.get_job_info(jobName)['lastBuild']['number']
-                    buil = server.get_build_info(jobName, lastBuildNumber)['building']
-                    # 判断是否正在构建
                     print ("\033[1;34m %s %s 开始构建..... \033[0m"%(listname,jobName))
+                    server.build_job(jobName)
+                    queue_job=server.get_queue_info()
+                    while queue_job:
+                        queue_job=server.get_queue_info()
+                        for queue_building_job in queue_job:
+                            queue_name=queue_building_job['task']['name']
+                            if queue_name!=jobName:
+                                break
+                    
+                    lastBuildNumber = server.get_job_info(jobName)['lastBuild']['number']
+                    #获取实时构建输出日志
+                    buil = server.get_build_info(jobName, lastBuildNumber)['building']
                     while buil:
-                        time.sleep(1)
-                        buil = server.get_build_info(jobName, lastBuildNumber)['building']
-
+                            time.sleep(1)
+                            buil = server.get_build_info(jobName, lastBuildNumber)['building']
                     console = server.get_build_console_output(jobName, lastBuildNumber)
+                    print ("\033[1;34m %s 构建序号：%s..... \033[0m"%(listname,lastBuildNumber))
                     print console
                     print ("\033[1;34m %s %s 构建成功..... \033[0m"%(listname,jobName))
             else:
-                print("\033[1;31m %s %s 不存在\033[0m"%(listname,jobName))
+                print("\033[1;31m %s %s 不存在\033[0m"%(listname,jobName))  
         except Exception, e:
-            print("\033[1;31m %s %s 登录失败.....\033[0m"%(listname,jkurl))
-
+            print("\033[1;31m %s %s 登录失败.....\033[0m"%(listname,jkurl)) 
+ 
 if __name__=='__main__':
-    user = ""
-    passwd = ""
+    user = "qtqky"
+    passwd = "Ghyfv#%5896"
     # 构建环境参数
     deployenv = sys.argv[1]
     # 构建应用参数
@@ -67,5 +87,6 @@ if __name__=='__main__':
     # 构建版本号
     version = sys.argv[3]
     confurl = "/data/ops/scripts/sendjkbuild/common/urls.ini"
-    param_dict = {"commit_version": version}
+    #"version" 这个key必须是对应job的参数名称，否则不起效
+    param_dict = {"version": version}
     buildjob()
