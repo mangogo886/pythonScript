a6725b77da6d7162ca3f3a6576f2c383575c5351
zhumin <zhum98@126.com>
2020-10-28 16:55:43 +0800

    v2,增加获取构建结果，进行判断是否构建成功

--- sendjenkins/sendjkbuild/batchBuildApp/batchBuildApp.py
+++ sendjenkins/sendjkbuild/batchBuildApp/batchBuildApp.py
@@ -23,15 +23,11 @@ def buildjob():
         try:
             server = jenkins.Jenkins(jkurl, username=user, password=passwd,timeout =20)
             if server.job_exists(name=jobName):
-                #获取job信息
                 jobinfo = server.get_job_info(jobName)
-                #判断是否设置了构建参数
                 if jobinfo['actions'][0]:
-                    #执行build
                     print ("\033[1;34m %s %s 开始构建..... \033[0m"%(listname,jobName))
                     server.build_job(jobName,parameters=param_dict)
                     queue_job=server.get_queue_info()
-                    #判断执行的job是否处于pending状态
                     while queue_job:
                         queue_job=server.get_queue_info()
                         for queue_building_job in queue_job:
@@ -40,38 +36,24 @@ def buildjob():
                                 break
 
                     lastBuildNumber = server.get_job_info(jobName)['lastBuild']['number']
-                    #获取实时构建输出日志
                     buil = server.get_build_info(jobName, lastBuildNumber)['building']
                     while buil:
                             time.sleep(1)
                             buil = server.get_build_info(jobName, lastBuildNumber)['building']
+                          
                     console = server.get_build_console_output(jobName, lastBuildNumber)
+                    lastBuildResult=server.get_build_info(jobName,lastBuildNumber)['result']
                     print ("\033[1;34m %s 构建序号：%s..... \033[0m"%(listname,lastBuildNumber))
                     print console
-                    print ("\033[1;34m %s %s 构建成功..... \033[0m"%(listname,jobName))
+                    #print ("\033[1;34m %s %s 构建结果: %s..... \033[0m"%(listname,jobName,lastBuildResult))
+                    if lastBuildResult=="SUCCESS":
+                        print ("\033[1;34m %s %s 构建成功..... \033[0m"%(listname,jobName))
+                    else:
+                        print("\033[1;31m %s %s  构建失败.....\033[0m"%(listname,jobName))
            
                 else:
-                    print ("\033[1;34m %s %s 无参数构建.... \033[0m"%(listname,jobName))
-                    print ("\033[1;34m %s %s 开始构建..... \033[0m"%(listname,jobName))
-                    server.build_job(jobName)
-                    queue_job=server.get_queue_info()
-                    while queue_job:
-                        queue_job=server.get_queue_info()
-                        for queue_building_job in queue_job:
-                            queue_name=queue_building_job['task']['name']
-                            if queue_name!=jobName:
-                                break
-                    
-                    lastBuildNumber = server.get_job_info(jobName)['lastBuild']['number']
-                    #获取实时构建输出日志
-                    buil = server.get_build_info(jobName, lastBuildNumber)['building']
-                    while buil:
-                            time.sleep(1)
-                            buil = server.get_build_info(jobName, lastBuildNumber)['building']
-                    console = server.get_build_console_output(jobName, lastBuildNumber)
-                    print ("\033[1;34m %s 构建序号：%s..... \033[0m"%(listname,lastBuildNumber))
-                    print console
-                    print ("\033[1;34m %s %s 构建成功..... \033[0m"%(listname,jobName))
+                    print("\033[1;31m %s %s 构建失败,没有设置构建参数version,请到对应环境修改后再重新执行.....\033[0m"%(listname,jobName))
+                 
             else:
                 print("\033[1;31m %s %s 不存在\033[0m"%(listname,jobName))  
         except Exception, e:
@@ -87,6 +69,5 @@ if __name__=='__main__':
     # 构建版本号
     version = sys.argv[3]
     confurl = "/data/ops/scripts/sendjkbuild/common/urls.ini"
-    #"version" 这个key必须是对应job的参数名称，否则不起效
     param_dict = {"version": version}
     buildjob()
