dda0ee6c9c9343ea16c6881453112ceec8c218fb
zhumin <zhum98@126.com>
2020-10-30 08:52:30 +0800

    修复因为构建失败无法获取job信息的问题

--- sendjenkins/sendjkbuild/jkversion/jobversion-v2.py
+++ sendjenkins/sendjkbuild/jkversion/jobversion-v2.py
@@ -7,48 +7,9 @@ import sys
 import jenkins
 import time
 import ConfigParser
+sys.path.append("/data/ops/scripts/sendjkbuild")
+from common.chinaName import *
 
-
-def envname(list):
-    if list=="hw":
-        name="华为云"
-    elif list=="nsfz":
-        name="南师附中"
-    elif list=="xxzx":
-        name="湘西中学"
-    elif list=="xxxx":
-        name="湘西小学"
-    elif list=="xxyey":
-        name="湘西幼儿园"
-
-    elif list=="gm":
-        name="高密中学"
-    elif list=="gm":
-        name="高密中学"
-    elif list=="rq":
-        name="瑞泉中学"
-    elif list=="wnzx":
-        name="渭南中学"
-    elif list=="hsxx":
-        name="护士学校"
-    elif list=="snzx":
-        name="思南中学"
-
-    elif list=="zsys":
-        name="中山一中"
-    elif list=="gzjl":
-        name="广州金隆"
-    elif list=="sdlj":
-        name="顺德九江"
-    elif list=="xa30":
-        name="西安30中"
-    elif list=="gzqh":
-        name="贵阳清华中学"
-
-    else:
-        name="没有匹配名字"
-
-    return name
 
 def get_jkurl(priurl):
     confurl="/data/ops/scripts/sendjkbuild/common/urls.ini"
@@ -68,28 +29,50 @@ def get_job_version(na):
         return
     else:
         try:
-            server = jenkins.Jenkins(jkurl, username='', password='')
+            server = jenkins.Jenkins(jkurl, username='qtqky', password='Ghyfv#%5896')
             jobs_list=server.get_all_jobs()
             for jobs in jobs_list:
+
                 job_name_list=jobs['fullname']
                 if job_name_list.startswith("qky-"):
                     job_name=job_name_list
+ 
                     if na in job_name:
                         if server.job_exists(name=job_name):
+                            
                             jobInfo = server.get_job_info(job_name)
+                          
                             if server.get_job_info(job_name)['lastBuild'] is not None:
                                 lastBuildNumber = server.get_job_info(job_name)['lastBuild']['number']
-                                Buildtemp = max(server.get_build_info(job_name, lastBuildNumber)['actions'])
-                                if Buildtemp.has_key('lastBuiltRevision'):
-                                    Buildversion=Buildtemp['lastBuiltRevision']['SHA1']
-                                    tempTime=server.get_build_info(job_name,lastBuildNumber)['timestamp']
+                                buildResult=server.get_build_info(job_name, lastBuildNumber)['result']
+                                #先判断构建结果是否成功
+                                if buildResult!="FAILURE":
+                                    Buildtemp = max(server.get_build_info(job_name, lastBuildNumber)['actions'])
+                                    if Buildtemp.has_key('lastBuiltRevision'):
+                                        Buildversion=Buildtemp['lastBuiltRevision']['SHA1']
+                                        tempTime=server.get_build_info(job_name,lastBuildNumber)['timestamp']
+                                        buildtimestamp=int(str(tempTime)[0:10])
+                                        timeArray = time.localtime(buildtimestamp)
+                                        buildTime=time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+                                        print "部署环境: ",envname(priurl)
+                                        print "项目名称: ",job_name
+                                        print "版本号:" ,Buildversion
+                                        print "构建时间: ",buildTime
+                                        print "构建状态: ","\033[1;32m %s \033[0m"%buildResult
+                                    
+                                else:
+                                    tempTime=server.get_build_info(job_name, lastBuildNumber)['timestamp']
                                     buildtimestamp=int(str(tempTime)[0:10])
                                     timeArray = time.localtime(buildtimestamp)
                                     buildTime=time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-                                    print "部署环境:",envname(priurl)
-                                    print "项目名称:",job_name
-                                    print "版本号:",Buildversion
-                                    print "构建时间:",buildTime
+                                    failureversion=server.get_build_info(job_name, lastBuildNumber)['actions'][0]['parameters'][0]['value']
+                                    print "部署环境: ",envname(priurl)
+                                    print "项目名称: ",job_name
+                                    print "版本号：", failureversion
+                                    print "构建时间: ",buildTime
+                                    print "构建状态: ","\033[1;31m %s \033[0m"%buildResult
+                                    
+                              
         except Exception, e:
             print "jenkins地址无法登录:",jkurl
 
