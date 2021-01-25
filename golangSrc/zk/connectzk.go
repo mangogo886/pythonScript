95a1346d231f5997423115a7cd528756f29159cb
zhumin <zhum98@126.com>
2021-01-25 09:51:58 +0800

    修改判断逻辑

--- golangSrc/zk/connectzk.go
+++ golangSrc/zk/connectzk.go
@@ -1,60 +1,60 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/samuel/go-zookeeper/zk"
 	"github.com/toolkits/file"
 	"log"
-	"encoding/json"
-	_"github.com/go-sql-driver/mysql"
-	"github.com/samuel/go-zookeeper/zk"
-	"database/sql"
+	"net"
+	"os"
 	"strings"
 	"time"
-	//"os"
-	"net"
 )
 
 type Config struct {
 	Zkaddress string `json:"zkaddress"`
 	Databases string `json:"databases"`
-	Listen string `json:"listen"`
+	Listen    string `json:"listen"`
 }
 
-func ParseConfig() (string,string,string){
+func ParseConfig() (string, string, string) {
 	//linux环境改这个方式
-	//filepath,_:=os.Getwd()
-	//filename:="config.json"
-	cfg:="E:\\golangworkspace\\src\\connectzk\\config.json"
-	//cfg:=filepath+"\\"+filename
+	filepath, _ := os.Getwd()
+	filename := "config.json"
+	//cfg:="E:\\golangworkspace\\src\\connectzk\\config.json"
+	cfg := filepath + "/" + filename
 
-	if !file.IsExist(cfg){
-		log.Println(cfg,"is not existent")
+	if !file.IsExist(cfg) {
+		log.Println(cfg, "is not existent")
 	}
 	var c Config
 	configContent, err := file.ToTrimString(cfg)
-	err = json.Unmarshal([]byte(configContent),&c)
-	if err!=nil{
-		log.Println("parse config file:", cfg, "fail:",err)
+	err = json.Unmarshal([]byte(configContent), &c)
+	if err != nil {
+		log.Println("parse config file:", cfg, "fail:", err)
 	}
-	log.Println("read config file:", cfg, "successfully")
-   return c.Zkaddress,c.Databases,c.Listen
+
+	return c.Zkaddress, c.Databases, c.Listen
 }
 
-func syszk(){
-	var flags int32=0
-	var acls=zk.WorldACL(zk.PermAll)
+func syszk() {
+	var flags int32 = 0
+	var acls = zk.WorldACL(zk.PermAll)
 	var DB *sql.DB
-	zkhost,dburl,_:=ParseConfig()
-
-	zkconn,_,err:=zk.Connect([]string{zkhost},time.Second*5)
-	if err!=nil{
-		log.Println(zkhost,err)
+	zkhost, dburl, _ := ParseConfig()
+	zkconn, _, err := zk.Connect([]string{zkhost}, time.Second*5)
+	if err != nil {
+		log.Println(err)
 	}
-	DB,_=sql.Open("mysql",dburl)
-	if err:=DB.Ping();err!=nil{
+
+	DB, _ = sql.Open("mysql", dburl)
+	if err := DB.Ping(); err != nil {
 		log.Println("数据库连接失败")
 	}
-	row,err:=DB.Query("select appCode,path,content,mark from zk_node;")
-	if err!=nil{
+	row, err := DB.Query("select appCode,path,content,mark from zk_node;")
+	if err != nil {
 		log.Println(err)
 	}
 
@@ -69,44 +69,43 @@ func syszk(){
 		_ = row.Scan(&appCode, &path, &content, &mark)
 		//把路径分割成数组
 		splitNode := strings.Split(path, "/")
-		//遍历每个分割的数组
-		for _, node := range splitNode {
-			//拼接路径，递归创建节点
-			tempnode += "/" + node
-			nodepath := "/" + strings.TrimLeft(tempnode, "/")
-			nodebool, _, _ := zkconn.Exists(nodepath)
-			if nodebool {
-				zkconn.Set(nodepath, nil, flags)
+		if mark == 0 {
 
-			} else {
-				zkconn.Create(nodepath, nil, flags, acls)
+			//遍历每个分割的数组
+			for _, node := range splitNode {
+				//拼接路径，递归创建节点
+				tempnode += "/" + node
+				nodepath := "/" + strings.TrimLeft(tempnode, "/")
+				nodebool, _, _ := zkconn.Exists(nodepath)
+				if !nodebool {
+					//zkconn.Set(nodepath, nil, flags)
+					zkconn.Create(nodepath, nil, flags, acls)
+				}
 			}
-		}
-		nodebool1,_,_:=zkconn.Exists(path)
-		if nodebool1{
+			//nodebool1,_,_:=zkconn.Exists(path)
 			//修改节点值，需要先获取节点状态，再通过Version去修改，否则无法修改
-			_,state,_:=zkconn.Get(path)
-			if mark==0{
-				zkconn.Set(path,[]byte(content),state.Version)
-				_,_=DB.Exec("update zk_node set mark=1 where mark=? and path=?",0,path)
-				log.Println(path,"成功同步到zookeeper..........",content)
-			}
+			_, state, _ := zkconn.Get(path)
+			zkconn.Set(path, []byte(content), state.Version)
+			_, _ = DB.Exec("update zk_node set mark=1 where mark=? and path=?", 0, path)
+			log.Println(path, "成功同步到zookeeper..........", content)
+			//
 		}
 	}
 	zkconn.Close()
 	DB.Close()
-	}
+}
 
-func main(){
-	_,_,ip:=ParseConfig()
-	listen,err:=net.Listen("tcp",ip)
-	if err!=nil{
+func main() {
+	_, _, ip := ParseConfig()
+	listen, err := net.Listen("tcp", ip)
+	if err != nil {
 		log.Println(err)
 	}
-	log.Println("监听地址:",ip)
+	log.Println("监听地址:", ip)
+	log.Println("read config file: cfg successfully")
 	defer listen.Close()
-	for{
-		time.Sleep(time.Second*15)
+	for {
+		time.Sleep(time.Second * 15)
 		syszk()
 	}
 }
