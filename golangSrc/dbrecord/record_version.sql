7799d7fed03a3386a8d9cf7bc88e5437a69de93e
zhumin <zhum98@126.com>
2021-01-29 17:29:11 +0800

    a

--- golangSrc/dbrecord/record_version.sql
+++ golangSrc/dbrecord/record_version.sql
@@ -1,9 +1,8 @@
-CREATE TABLE `zk_node` (
-   `id`  bigint(20)NOT NULL AUTO_INCREMENT,
-  `appCode`	 varchar(255) DEFAULT NULL,
-  `path` varchar(255) DEFAULT NULL,
-  `content` varchar(5000) DEFAULT NULL,
-  `mark`  int(1) NOT NULL  DEFAULT '1',
-  `update_time`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  primary key(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+CREATE TABLE `record_version` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `school` varchar(255) DEFAULT NULL,
+  `appCode` varchar(255) DEFAULT NULL,
+  `sqlfile` varchar(5000) DEFAULT NULL,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
