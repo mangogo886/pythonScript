2a1dc9741c21517af759853f5fe2946e84e021e6
zhumin <zhum98@126.com>
2021-02-01 08:42:07 +0800

    修改post地址

--- golangSrc/controldb/new/controldb.go
+++ golangSrc/controldb/new/controldb.go
@@ -1,105 +1,105 @@
 package main
 
 import (
-        "database/sql"
-        "encoding/json"
-        "fmt"
-        _ "github.com/go-sql-driver/mysql"
-        "github.com/toolkits/file"
-        "io/ioutil"
-        "log"
-        "net/http"
-        url2 "net/url"
-        "os"
-        "strings"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/toolkits/file"
+	"io/ioutil"
+	"log"
+	"net/http"
+	url2 "net/url"
+	"os"
+	"strings"
 )
 
 type Config struct {
-        User     string `json:"user"`
-        Password string `json:"password"`
-        Address  string `json:"address"`
-        School   string `json:"schoolName"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Address  string `json:"address"`
+	School   string `json:"schoolName"`
 }
 
 func ParseConfig() (string, string, string, string) {
-        //linux环境改这个方式
-        filepath, _ := os.Getwd()
-        filename := "config.json"
-        //cfg := "E:\\golangworkspace\\src\\connmysql\\config1.json"
-        cfg := filepath + "/" + filename
+	//linux环境改这个方式
+	filepath, _ := os.Getwd()
+	filename := "config.json"
+	//cfg := "E:\\golangworkspace\\src\\connmysql\\config1.json"
+	cfg := filepath + "/" + filename
 
-        if !file.IsExist(cfg) {
-                log.Println(cfg, "is not existent")
-        }
-        var c Config
-        configContent, err := file.ToTrimString(cfg)
-        err = json.Unmarshal([]byte(configContent), &c)
+	if !file.IsExist(cfg) {
+		log.Println(cfg, "is not existent")
+	}
+	var c Config
+	configContent, err := file.ToTrimString(cfg)
+	err = json.Unmarshal([]byte(configContent), &c)
 
-        if err != nil {
-                log.Println("parse config file:", cfg, "fail:", err)
-        }
+	if err != nil {
+		log.Println("parse config file:", cfg, "fail:", err)
+	}
 
-        return c.User, c.Password, c.Address, c.School
+	return c.User, c.Password, c.Address, c.School
 }
 
 func runsql() {
-        args := os.Args
-        if args == nil || len(args) < 2 {
-                log.Println("please entry exec dbname  and sqlfile...")
-                return
-        }
-        dbname := args[1]
-        filename := args[2]
-        var DB *sql.DB
-        u, p, url, app := ParseConfig()
-        //linux环境改这个方式
-        filepath, _ := os.Getwd()
-        sqlfile := filepath + "/" + filename
-        //sqlfile := "E:\\golangworkspace\\src\\connmysql\\" + filename
-        f, err := os.Open(sqlfile)
-        //file, err := ioutil.ReadFile(sqlfile)
-        if err != nil {
-                log.Println("找不到sql文件>>>>>", err)
-                return
-        }
-        file, _ := ioutil.ReadAll(f)
-        dblink := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", u, p, url, dbname)
-        DB, err = sql.Open("mysql", dblink)
-        if err != nil {
-                log.Println("数据库连接失败.....")
-                return
-        }
-        datalist := strings.Split(string(file), ";")
+	args := os.Args
+	if args == nil || len(args) < 2 {
+		log.Println("please entry exec dbname  and sqlfile...")
+		return
+	}
+	dbname := args[1]
+	filename := args[2]
+	var DB *sql.DB
+	u, p, url, app := ParseConfig()
+	//linux环境改这个方式
+	filepath, _ := os.Getwd()
+	sqlfile := filepath + "/" + filename
+	//sqlfile := "E:\\golangworkspace\\src\\connmysql\\" + filename
+	f, err := os.Open(sqlfile)
+	//file, err := ioutil.ReadFile(sqlfile)
+	if err != nil {
+		log.Println("找不到sql文件>>>>>", err)
+		return
+	}
+	file, _ := ioutil.ReadAll(f)
+	dblink := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", u, p, url, dbname)
+	DB, err = sql.Open("mysql", dblink)
+	if err != nil {
+		log.Println("数据库连接失败.....")
+		return
+	}
+	datalist := strings.Split(string(file), ";")
 
-        for _, data := range datalist {
-                data1 := strings.TrimSpace(data)
-                if data1 != "" {
-                        _, err := DB.Exec(data1)
-                        if err != nil {
-                                fmt.Printf("%s 出现报错>>>>>%s\n", data1, err)
-                        }
-                }
-        }
-        DB.Close()
-        f.Close()
-        //post数据到接口，生成一条数据库记录
-        msg := map[string]string{"schoolname": app, "appCode": dbname, "sqlfile": filename}
-        DataUrlVal := url2.Values{}
-        for k, v := range msg {
-                DataUrlVal.Add(k, v)
-        }
+	for _, data := range datalist {
+		data1 := strings.TrimSpace(data)
+		if data1 != "" {
+			_, err := DB.Exec(data1)
+			if err != nil {
+				fmt.Printf("%s 出现报错>>>>>%s\n", data1, err)
+			}
+		}
+	}
+	DB.Close()
+	f.Close()
+	//post数据到接口，生成一条数据库记录
+	msg := map[string]string{"schoolname": app, "appCode": dbname, "sqlfile": filename}
+	DataUrlVal := url2.Values{}
+	for k, v := range msg {
+		DataUrlVal.Add(k, v)
+	}
 
-        client := &http.Client{}
-        req, _ := http.NewRequest("POST", "http://127.0.0.1:9999/version", strings.NewReader(DataUrlVal.Encode()))
-        req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-        resp, err := client.Do(req)
-        if err != nil {
-                fmt.Println("接口报错>>>", err)
-        }
-        resp.Body.Close()
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", "http://vs.qky100.cn/version", strings.NewReader(DataUrlVal.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("接口报错>>>", err)
+	}
+	resp.Body.Close()
 
 }
 
 func main() {
-        runsql()
+	runsql()
 }
--- golangSrc/controldb/new/readme.txt
+++ golangSrc/controldb/new/readme.txt
@@ -1,2 +1,4 @@
-1.新版本，执行完sql之后，不管是否成功，都往接口地址: vs.qky100.cn/version(需配置host)，添加一条记录，使用POST方法，传送三个字段schoolname（环境名称），appCode（执行的数据库），sqlfile(执行的sql文件),接口地址写死在代码
+1.新版本，执行完sql之后，不管是否成功，都往接口地址: vs.qky100.cn/version(需配置host)，添加一条记录，
+使用POST方法，传送三个字段：
+schoolname（环境名称），appCode（执行的数据库），sqlfile(执行的sql文件),接口地址写死在代码
 2.执行时，需要传入两个参数，第一是执行的数据库名称，第二是执行的sql文件名称
